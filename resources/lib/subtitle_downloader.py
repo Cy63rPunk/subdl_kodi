b58d192172dffadd9644922718ce8ff172fe4876
Cy63rPunk <38062780+Cy63rPunk@users.noreply.github.com>
2024-07-29 09:06:12 +0200

    Update subtitle_downloader.py

--- resources/lib/subtitle_downloader.py
+++ resources/lib/subtitle_downloader.py
@@ -79,7 +79,7 @@ class SubtitleDownloader:
 
 
         try:
-            self.subtitles = self.open_subtitles.search_subtitles(media_data)
+            self.subtitles = self.open_subtitles.search_subtitles(media_data, language_data)
         # TODO handle errors individually. Get clear error messages to the user
         except (TooManyRequests, ServiceUnavailable, ProviderError, ValueError) as e:
             if (str(e) == "can't find movie or tv"):
